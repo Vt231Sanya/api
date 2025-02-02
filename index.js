befad8e6cfa7af9dfa42acd53f9668008038a55e
Нагапетян Олександр Олександрович <vt231_noo@student.ztu.edu.ua>
2025-02-02 03:09:39 +0200

    Second

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 app.use(bodyParser.json());
 
-let items = [];
+const SECRET_KEY = 'ILoveAlina';
+
+let items = [
+    {
+        id: 1,
+        name: 123,
+        description: 123
+    }
+];
 let idCounter = 1;
 
+const users = [
+    { id: 1, username: 'admin', password: 'admin', role: 'Admin' },
+    { id: 2, username: 'user', password: 'user', role: 'User' }
+];
+
+function authenticateToken(req, res, next) {
+    const token = req.header('Authorization');
+    if (!token) return res.status(401).json({ message: 'Access denied' });
+
+    jwt.verify(token.split(' ')[1], SECRET_KEY, (err, user) => {
+        if (err) return res.status(403).json({ message: 'Invalid token' });
+        req.user = user;
+        next();
+    });
+}
+
+function authorizeRole(role) {
+    return (req, res, next) => {
+        if (req.user.role !== role) {
+            return res.status(403).json({ message: 'Forbidden' });
+        }
+        next();
+    };
+}
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    const user = users.find(u => u.username === username && u.password === password);
+    if (!user) return res.status(401).json({ message: 'Invalid credentials' });
+
+    const token = jwt.sign({ id: user.id, username: user.username, role: user.role }, SECRET_KEY, { expiresIn: '1h' });
+    res.json({ token });
+});
+
+// CRUD операції
 app.get('/items', (req, res) => {
     res.json(items);
 });
 
-app.post('/items', (req, res) => {
+app.post('/items', authenticateToken, authorizeRole('User'), (req, res) => {
     const newItem = {
         id: idCounter++,
         name: req.body.name,
@@ -27,7 +71,7 @@ app.get('/items/:id', (req, res) => {
     res.json(item);
 });
 
-app.patch('/items/:id', (req, res) => {
+app.patch('/items/:id', authenticateToken, authorizeRole('Admin'), (req, res) => {
     const item = items.find(i => i.id === parseInt(req.params.id));
     if (!item) return res.status(404).json({ message: 'Item not found' });
 
@@ -37,7 +81,7 @@ app.patch('/items/:id', (req, res) => {
     res.json(item);
 });
 
-app.delete('/items/:id', (req, res) => {
+app.delete('/items/:id', authenticateToken, authorizeRole('Admin'), (req, res) => {
     const itemIndex = items.findIndex(i => i.id === parseInt(req.params.id));
     if (itemIndex === -1) return res.status(404).json({ message: 'Item not found' });
 
